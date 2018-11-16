355f962c2e5c01981579db337643b10a512f6b59
Yony <arleybri18@gmail.com>
2018-11-15 22:26:59 -0500

    Se crea autenticacion de usuario y pagina de inicio del usuario

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+    def new
+        
+    end
+
+    def create
+        dog = Dog.find_by(name: params[:session][:name])
+        if dog && dog.authenticate(params[:session][:password])
+          log_in dog #metodo que esta en el helper se le envia el dog
+          redirect_to '/'
+        else
+          render 'new'
+        end
+    end
+
+    def destroy
+    end
 end
